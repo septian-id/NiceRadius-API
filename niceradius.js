4f880e8ef219e085646b6e1d3d2c6b08d35a1d63
Septiana Harun <55664528+septian-id@users.noreply.github.com>
2024-09-25 21:41:41 +0700

    Add files via upload

---
+++ niceradius.js
@@ -0,0 +1,40 @@
+//==================================================CONFIG====================================
+const apiKey = 'API_KEY_ANDA';
+const routerID = 'ROUTER_ID';
+//==================================================CONFIG====================================
+const apiUrl = 'https://niceradius.com/api';
+async function getData(path){
+	return new Promise(resolve => {
+		$.ajax({
+			type: "post",
+			url: apiUrl + '/' + path,
+			data: {apiKey: apiKey, routerID: routerID},
+			success: (data) => {resolve(data);}
+		})
+	})
+}
+
+async function getPaymentMethod(itemID){
+	return new Promise(resolve => {
+		$.ajax({
+			type: "post",
+			url: apiUrl + '/payment_method',
+			data: {apiKey: apiKey, item_id: itemID},
+			success: (data) => {resolve(data);}
+		})
+	});
+}
+
+async function createInvoice(params){
+	params.apiKey = apiKey;
+	return new Promise(resolve => {
+		$.ajax({
+			url: apiUrl + '/checkout',
+			type: 'POST',
+			data: params,
+			success: (data) => {
+				resolve(data);
+			}
+		});
+	})
+}
